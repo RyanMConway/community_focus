d57ef6a503729e0b8b36fd94a10fe1ed8e4b46e8
Ryan Conway <Rconwayak@gmail.com>
2026-01-28 20:09:50 -0500

    added photos back

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,7 +17,7 @@ const staff = [
     {
         name: "Lisa Austin",
         title: "Association Services",
-        image: null,
+        image: "/team/lisa-austin.jpg",
         bio: "Providing essential support to ensure our communities run smoothly.",
         email: "Lisa@CommunityFocusNC.com",
         phone: "(919) 564-9134 ext. 106"
@@ -33,7 +33,7 @@ const staff = [
     {
         name: "Amy Ghiloni",
         title: "Community Association Manager",
-        image: null,
+        image: "/team/amy-ghiloni.png",
         bio: "Dedicated to supporting board members and homeowners with clear communication.",
         email: "Amy@CommunityFocusNC.com",
         phone: "(919) 564-9134 ext. 109"
@@ -73,7 +73,7 @@ const staff = [
     {
         name: "Wade Womble",
         title: "Community Association Manager",
-        image: null,
+        image: "/team/wade-womble.jpg",
         bio: "Focused on efficient community management and solving complex association challenges.",
         email: "Wade@CommunityFocusNC.com",
         phone: "(919) 564-9134 ext. 108"
